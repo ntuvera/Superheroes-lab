f4f30dab89019c4ea0754fe5db2c8f566064a697
ntuvera <ntuvera@gmail.com>
2019-10-01 20:22:38 -0400

    DONE: readFromFile(filename) and writeToFile(filename)

--- src/com/ga/superhero/Main.java
+++ src/com/ga/superhero/Main.java
@@ -1,18 +1,90 @@
 package com.ga.superhero;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
 
-	}
+        ArrayList<Superhero> superheroes = new ArrayList<Superhero>();
 
-	// TODO: Read File using BufferedReader
+        // TODO: run readfile and iterate to
+
+        /*readText*/
+//		for( Superhero hero : /* */) {
+//			superheroes.add(hero);
+//		}
+
+//        startQuiz();
+        String test = readFromFile("input.txt");
+        System.out.println(test);
+        String writeTest = writeToFile(("output.txt"));
+
+    }
 
 
-	// TODO: Write File using BufferedWriter
+    // TODO: Read File using BufferedReader
+    public static String readFromFile(String filename) throws IOException {
+//        try {
+//            List<String> allLines = Files.readAllLines(Paths.get("/input.txt"));
+//            for (String line : allLines) {
+//                System.out.println(line);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        String file = filename;
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        StringBuilder builder = new StringBuilder();
+        String currentLine = reader.readLine();
+        while (currentLine != null) {
+            builder.append(currentLine);
+            builder.append("\n");
+            currentLine = reader.readLine();
+        }
+        reader.close();
+        return builder.toString();
+    }
 
 
-	// TODO: Run game, loop through
+    // TODO: Write File using BufferedWriter
+    public static String writeToFile(String fileName) throws IOException {
+        String str = "Hello";
+
+        Path path = Paths.get(fileName);
+        byte[] strToBytes = str.getBytes();
+
+        Files.write(path, strToBytes);
+
+        String read = Files.readAllLines(path).get(0);
+        return read;
+    }
+
+
+
+    // TODO: Run game, loop through
+
+    public static String startQuiz(ArrayList<Superhero> superheroes) {
+        System.out.println("Welcome to the Superhero quiz");
+        int correctCount = 0;
+        int incorrectCount = 0;
+
+        for (Superhero hero : superheroes) {
+            System.out.println("Is " + hero.getSuperheroName() + " a DC or Marvel hero?");
+
+        }
+
+        return "Hello";
+    }
 
 }
--- src/com/ga/superhero/Superhero.java
+++ src/com/ga/superhero/Superhero.java
@@ -1,5 +1,40 @@
 package com.ga.superhero;
 
 public class Superhero {
+    private String superheroName;
+    private String secretIdentity;
+    private String home;
+    private String domain;
 
+    public String getSuperheroName() {
+        return superheroName;
+    }
+
+    public void setSuperheroName(String superheroName) {
+        this.superheroName = superheroName;
+    }
+
+    public String getSecretIdentity() {
+        return secretIdentity;
+    }
+
+    public void setSecretIdentity(String secretIdentity) {
+        this.secretIdentity = secretIdentity;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public void setHome(String home) {
+        this.home = home;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
 }
